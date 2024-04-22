a2c4b3722685ed975356a3c35b35747a2d81f38a
fatimah-mse <fatimahmse83@gmail.com>
2024-04-22 16:36:25 +0200

    add-slider

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+var swiper = new Swiper(".swiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    slidesPerView: "3",
+    coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+    },
+    pagination: {
+    el: ".swiper-pagination",
+    },
+    breakpoints: {
+    320: {
+        slidesPerView: 1,
+    },
+    640: {
+        slidesPerView: 1,
+    },
+    768: {
+        slidesPerView: 2,
+    },
+    1024: {
+        slidesPerView: 3,
+    },
+    }
+});
+
+function model_show () {
+    
+}
